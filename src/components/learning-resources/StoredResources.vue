d23d4a5570861825daef8c8ad0bdd9439d02f9ee
AvinashKomaturi <aviavinash0464@gmail.com>
2021-03-30 16:32:10 +0530

    vue-resource-manager

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in resources"
+      :key="resource.id" 
+      :res='resource'
+    ></learning-resource>
+  </ul>
+</template>
+
+
+<script>
+
+import LearningResource from './LearningResource';
+export default {
+  inject:['resources'],
+  components:{
+    'learning-resource' : LearningResource
+  },
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+  }
+</style>
